2cde751a573229fe5885a56f673317f88cb194bf
cakwahyu <akun.zudi@gmail.com>
2022-03-24 08:27:13 +0700

    Upload

---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<?php require_once __DIR__ . '/../config/connect.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDW (Pertemuan ke 5) | LATIHAN CRUD PHP MYSQL</title>
+
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<body>
+    <h1>Latihan CRUD PHP dan Mysqli</h1>
---
+++ users/edit.php
@@ -0,0 +1,41 @@
+<?php require_once '../templates/header.php';
+
+$user = mysqli_query($conn, "SELECT * FROM users WHERE id = " . $_GET['id']);
+$user = mysqli_fetch_object($user);
+
+?>
+<h1>Edit User</h1>
+<a href="index.php">Kembali</a>
+<br>
+<br>
+<form action="" method="post">
+    <input type="hidden" name="id" value="<?= $user->id; ?>">
+    <p>
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" value="<?= $user->name; ?>">
+    </p>
+    <p>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" value="<?= $user->email; ?>">
+    </p>
+    <p>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" value="<?= $user->password; ?>">
+    </p>
+    <button type="submit" name="simpan">Perbarui</button>
+</form>
+<?php
+
+if (isset($_POST['simpan'])) {
+    $query = "UPDATE users SET
+        name = '" . $_POST['name'] . "', email = '" . $_POST['email'] . "', password = '" . $_POST['password'] . "'
+        WHERE id = " . $_POST['id'];
+
+    if (mysqli_query($conn, $query)) {
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+    } else {
+        echo "terjadi kesalahan query";
+    }
+}
+
+require_once '../templates/footer.php'; ?>
